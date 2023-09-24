1e81017b890e02cfde60c7bc492b52cad9c3c4bc
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-09-24 07:40:59 -0500

    Experiment with JLab kernel

--- src/tests/test_data_content.py
+++ src/tests/test_data_content.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+import duckdb
 
-def test_read_db(db_file:Path):
+
+def test_read_db(db_file):
     assert db_file.exists()
+    con = duckdb.connect(str(db_file))
+    r = con.sql('SHOW ALL TABLES;')
+    print(r)
