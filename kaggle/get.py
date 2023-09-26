47d36aa216529bf9aea650ad4199d118cd44fd24
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-09-26 14:16:04 -0500

    Downloading data from Kaggle.

--- kaggle/get.py
+++ kaggle/get.py
@@ -1,7 +1,22 @@
-import subprocess
+import shutil
 from pathlib import Path
 
+from kaggle import settings
 from kaggle.terminal_commands import run_commands
+
+
+def configure_kaggle(envs_folder: Path):
+    envs_file = envs_folder / 'kaggle.json'
+    if not envs_file.exists():
+        raise Exception('Kaggle configuration file not found')
+    kaggle_folder = Path.home() / '.kaggle'
+    kaggle_folder.mkdir(exist_ok=True)
+    config_file = kaggle_folder / 'kaggle.json'
+    if not config_file.exists():
+        shutil.copy(envs_file, config_file)
+        return True
+    else:
+        return False
 
 
 def download_dataset(dataset_name: str, download_folder: Path) -> Path:
@@ -18,6 +33,7 @@ def download_dataset(dataset_name: str, download_folder: Path) -> Path:
 
 if __name__ == '__main__':
     #  kaggle datasets download joebeachcapital/house-prices
+    configure_kaggle(settings.ENVS_FOLDER)
     dataset = 'joebeachcapital/house-prices-2001-2020'
     data_folder = Path(__file__).parent / 'data'
     data_folder.mkdir(exist_ok=True)
---
+++ kaggle/settings.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_FOLDER = Path(__file__).parent.parent
+
+APP_FOLDER = Path(__file__).parent
+
+ENVS_FOLDER = ROOT_FOLDER / '.envs'
