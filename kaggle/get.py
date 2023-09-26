4ae50bf508b2cd5e75412ce0e7dd4b01fd53c50c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-09-26 07:12:48 -0500

    Downloading data from Kaggle

--- kaggle/get.py
+++ kaggle/get.py
@@ -5,8 +5,7 @@ from kaggle.terminal_commands import run_commands
 
 
 def download_dataset(dataset_name: str, download_folder: Path) -> Path:
-    command_list = ['kaggle', 'datasets', 'download', dataset_name] #, '-p', str(download_folder)]
-    # command_list = ['which', 'kaggle']
+    command_list = ['kaggle', 'datasets', 'download', dataset_name, '-p', str(download_folder), '--unzip']
     results, errors = run_commands(command_list)
     for r in results:
         print(r)
@@ -24,6 +23,3 @@ if __name__ == '__main__':
     data_folder.mkdir(exist_ok=True)
 
     file = download_dataset(dataset_name=dataset, download_folder=data_folder)
-    # cmds = ['kaggle', 'datasets', 'download', dataset] #, '-p', str(download_folder)]
-    # d = subprocess.call(cmds)
-    # print(d)
