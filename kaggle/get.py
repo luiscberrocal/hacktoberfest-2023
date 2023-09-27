d84b29fe186dfad6fbeecf85b217b35edcdc547a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-09-27 06:21:59 -0500

    Getting data from Kaggle.

--- kaggle/get.py
+++ kaggle/get.py
@@ -19,8 +19,12 @@ def configure_kaggle(envs_folder: Path):
         return False
 
 
-def download_dataset(dataset_name: str, download_folder: Path) -> Path:
-    command_list = ['kaggle', 'datasets', 'download', dataset_name, '-p', str(download_folder), '--unzip']
+def download_dataset(owner: str, dataset_name: str, download_folder: Path) -> Path:
+    dataset = f'{owner}/{dataset_name}'
+    folder = download_folder / dataset_name
+    folder.mkdir(exist_ok=True)
+
+    command_list = ['kaggle', 'datasets', 'download', dataset, '-p', str(folder), '--unzip']
     results, errors = run_commands(command_list)
     for r in results:
         print(r)
@@ -34,8 +38,9 @@ def download_dataset(dataset_name: str, download_folder: Path) -> Path:
 if __name__ == '__main__':
     #  kaggle datasets download joebeachcapital/house-prices
     configure_kaggle(settings.ENVS_FOLDER)
-    dataset = 'joebeachcapital/house-prices-2001-2020'
+    ds_owner = 'fedesoriano'
+    ds_name = 'california-housing-prices-data-extra-features'
     data_folder = Path(__file__).parent / 'data'
     data_folder.mkdir(exist_ok=True)
 
-    file = download_dataset(dataset_name=dataset, download_folder=data_folder)
+    file = download_dataset(owner=ds_owner, dataset_name=ds_name, download_folder=data_folder)
