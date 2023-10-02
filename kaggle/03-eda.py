6a28328e7bef123fff585cb6c1da95eec698f529
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-02 18:08:41 -0500

    Scatterplot for locations.

--- kaggle/03-eda.py
+++ kaggle/03-eda.py
@@ -25,6 +25,10 @@ sns.heatmap(df.corr(), annot=True, cmap='YlGnBu')
 plt.show()
 
 # %%
+sns.scatterplot(data=df, x='latitude', y='longitude', hue='targetprice_in_lacs', palette='flare')
+plt.show()
+
+# %%
 df.hist(figsize=(12,9))
 plt.tight_layout()
 plt.show()
