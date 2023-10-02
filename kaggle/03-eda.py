b9b1d96d5ce373f368b834e652154ba76c8505c5
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-02 09:50:07 -0500

    EDA task not working.

--- kaggle/03-eda.py
+++ kaggle/03-eda.py
@@ -1,10 +1,19 @@
-# + tags=["parameters"]
+# %% tags=["parameters"]
 # declare a list tasks whose products you want to use as inputs
-upstream = ['03-eda']
+upstream = ['02-cleanup']
 product = None
 
-
 # %%
+import pandas as pd
 # log convert non gaussian columns?
 # df['x'] = np.log(df['x'] +1)
 # df.hist()
+
+# %%
+df = pd.read_csv(upstream['02-cleanup']['clean_csv'])
+
+df.head()
+
+# %%
+df.corr()
+
