3cc96fea28332bb7c8e458808f7ecb0a853e7cb6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-02 09:40:52 -0500

    Cleanup of categorical columns

--- kaggle/02-cleanup.py
+++ kaggle/02-cleanup.py
@@ -28,6 +28,28 @@ df = df.rename(columns=renamed_mapping)
 
 df.info()
 
+# %%
+df['posted_by'].value_counts()
+
+# %%
+df['bhk_or_rk'].value_counts()
+
+# %%
+df = pd.get_dummies(df, columns=['posted_by', 'bhk_or_rk'])
+
+# %%
+df.head()
+
+# %%
+df.info()
+
+# %%
+for c in df.columns:
+    if df[c].dtype == bool:
+        print(f'{c} {df[c].dtype}')
+        df[c] = df[c].astype(int)
+
+df.info()
 
 # %%
 # 1. hot shoe encoding
