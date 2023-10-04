d0f153be03ab7909eb3ad6152f1596178dd34db6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-04 07:37:11 -0500

    Added split and transform steps

--- kaggle/04-transform.py
+++ kaggle/04-transform.py
@@ -10,25 +10,14 @@ warnings.filterwarnings('ignore')
 
 # %%
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 # %%
 df = pd.read_csv(upstream['02-cleanup']['clean_csv'])
 
 df.head()
+# %%
+# Scaling
+# Log tranform
 
 # %%
-X = df.drop(['price'], axis=1)
-y = df['price']
-
-# %%
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# %%
-train_data = X_train.join(y_train)
-test_data = X_test.join(y_test)
-
-# %%
-train_data.to_csv(product['train_data'], index=False)
-test_data.to_csv(product['test_data'], index=False)
+df.to_csv(product['transformed_csv'], index=False)
