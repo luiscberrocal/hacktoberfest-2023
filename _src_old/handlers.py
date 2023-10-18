7f4f72492c1f8c5d92c708f1f4d3c35f98b9ef3e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-18 12:20:10 -0400

    Delete previous src folders.

--- _src_old/handlers.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-def find_csv_file(folder: Path, csv_name: str) -> Path:
-    csvs = folder.glob('**/*.csv')
-    for csv in csvs:
-        if csv.name == csv_name:
-            return csv
