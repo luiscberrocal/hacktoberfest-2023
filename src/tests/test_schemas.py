4911382c6688a20d1c80550da89ed5726345dc29
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-22 10:29:46 -0500

    Returning House object.

--- src/tests/test_schemas.py
+++ src/tests/test_schemas.py
@@ -1,12 +1,7 @@
-from fastapi import FastAPI
-
 from src.app.schema import House
 
-app = FastAPI()
 
-
-@app.get('/')
-def main_end_point() -> House:
+def test_create_house():
     house_data = {
         'median_income': 3.87,
         "median_age": 28.6,
@@ -23,4 +18,5 @@ def main_end_point() -> House:
         "distance_to_sanfrancisco": 346_000.0,
     }
     house = House(**house_data)
-    return house
+
+    assert house
