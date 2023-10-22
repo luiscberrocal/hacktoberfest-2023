197aa5dceac93efdd6f102277f6f36383acef991
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-22 09:50:33 -0500

    Loading host files working.

--- src/app/app.py
+++ src/app/app.py
@@ -5,4 +5,7 @@ app = FastAPI()
 
 @app.get('/')
 def main_end_point():
-    return {'hello': 'world', "payload": "test", 'dynamic333': True}
+    return {'hello': 'world', "payload": "test",
+            'dynamic333': True,
+            "new_content" : "KKK"
+            }
