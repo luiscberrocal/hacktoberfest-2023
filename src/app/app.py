81334a05d98bd76ed38daf501ad7c4f28bb3df34
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-10-22 10:22:55 -0500

    Sample House object.

--- src/app/app.py
+++ src/app/app.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 
+from src.app.schema import House
+
 app = FastAPI()
 
 
 @app.get('/')
-def main_end_point():
-    return {'hello': 'world', "payload": "test",
-            'dynamic333': True,
-            "new_content" : "KKK"
-            }
+def main_end_point() -> House:
+    house_data = {
+        'mean_income': 3.87,
+        "median_age": 28.6,
+        "tot_rooms": 5,
+        "tot_bedrooms": 3,
+        "population": 1425,
+        "households": 500,
+        "latitude": 35.6,
+        "longitude": -119.56,
+        "distance_to_coast": 40_509.3,
+        "distance_to_la": 269_422,
+        "distance_to_sandiego": 398_000,
+        "distance_to_sanjose": 34_000.0,
+        "distance_to_sanfrancisco": 346_000.0,
+    }
+    house = House(**house_data)
+    return house
